2ac275f4c942d71b0cb0e33428858abc03e8b4a6
Brillian Musyafa <brillianmusyafa@gmail.com>
2019-12-30 10:02:14 +0700

    edit if setting table not exist

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,7 +7,6 @@ use Illuminate\Support\Facades\Schema;
 use View;
 use App\Setting;
 
-
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -17,11 +16,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        view()->share('app_name', $this->getSetting('app_name'));
-        view()->share('latitude_centre', $this->getSetting('latitude_centre'));
-        view()->share('longitude_centre', $this->getSetting('longitude_centre'));
-        view()->share('set_zoom', $this->getSetting('set_zoom'));
-        view()->share('gmaps_api_key', $this->getSetting('gmaps_api_key'));
+        if(Schema::hasTable('setting')){
+            view()->share('app_name', $this->getSetting('app_name'));
+            view()->share('latitude_centre', $this->getSetting('latitude_centre'));
+            view()->share('longitude_centre', $this->getSetting('longitude_centre'));
+            view()->share('set_zoom', $this->getSetting('set_zoom'));
+            view()->share('gmaps_api_key', $this->getSetting('gmaps_api_key'));
+        }
         Schema::defaultStringLength(191);
     }
 
